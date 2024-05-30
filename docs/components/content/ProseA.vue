1edc69017195545fcb0e7cc3526c3a8ae45e738b
paperplane110 <1374736640@qq.com>
2024-05-31 00:56:01 +0800

    ch1,p7,done

---
+++ docs/components/content/ProseA.vue
@@ -0,0 +1,27 @@
+<template>
+    <NuxtLink
+      :href="href"
+      :target="target"
+    >
+      <b>
+          <slot />
+      </b>
+      <Icon class="mr-1 mb-1" name="ph:link-bold"/>
+    </NuxtLink>
+  </template>
+  
+  <script setup lang="ts">
+  import type { PropType } from 'vue'
+  
+  defineProps({
+    href: {
+      type: String,
+      default: ''
+    },
+    target: {
+      type: String as PropType<'_blank' | '_parent' | '_self' | '_top' | (string & object) | null | undefined>,
+      default: undefined,
+      required: false
+    }
+  })
+  </script>
--- docs/components/content/SectionBreak.vue
+++ docs/components/content/SectionBreak.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex justify-center gap-8 my-16">
+    <div class="flex justify-center gap-8 my-16 text-2xl">
         <slot name="one"/>
         <slot name="two"/>
         <slot name="three"/>
