4ccc7130e20196c8d9d87c4eddb7debdaf4a3858
Tianyu Yuan <62664574+paperplane110@users.noreply.github.com>
2024-05-31 09:00:25 +0000

    Add description and header component

---
+++ docs/components/content/ChapterHeader.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1><slot/></h1>
+        <ProseBlockquote>{{ page.description }}</ProseBlockquote>
+        <br/>
+    </div>
+</template>
+
+<script setup lang="ts">
+const { page } = useContent()
+</script>
