9d98a0c7e689ed1b522cc68f1d0673327fcc55e0
paperplane110 <1374736640@qq.com>
2024-06-05 00:41:21 +0800

    ch1,page16,p3

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,7 +1,13 @@
 export default defineNuxtConfig({
   extends: 'content-wind',
-  modules: ["nuxt-gtag"],
+  modules: ["nuxt-gtag", "@nuxtjs/google-fonts"],
   gtag: {
     id: 'G-Y1VX13XEW7'
+  },
+  googleFonts: {
+    families: {
+      'Ma Shan Zheng': true,
+      'DotGothic16': true
+    }
   }
 })
---
+++ docs/tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    theme: {
+      fontFamily: {
+        'think': ['Ma Shan Zheng'],
+        'pixel': ['DotGothic16']
+      }
+    }
+  }
