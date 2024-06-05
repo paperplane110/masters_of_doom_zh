c1b22583a5c2e2a788941bb95d99e82953c1b433
paperplane110 <1374736640@qq.com>
2024-06-05 23:04:53 +0800

    ch2,p18,p2

---
+++ docs/components/content/Doing.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="border dark:border-gray-600 rounded-lg text-center bg-slate-50 dark:bg-slate-800">
+        <h3>Oops! 你读得好快啊！</h3>
+        <p>
+            更多内容正在努力翻译中 💬<br/>
+            请耐心等待，谢谢 🤗<br/>
+            订阅加星 
+            <NuxtLink to="https://github.com/paperplane110/masters_of_doom_zh">
+                <Icon name="mdi:github" />
+            </NuxtLink>，更新不错过
+        </p>
+    </div>
+</template>
--- docs/components/content/PrevNext.vue
+++ docs/components/content/PrevNext.vue
@@ -8,15 +8,17 @@ const [prev, next] = await queryContent('/')
 
 <template>
   <div class="grid grid-cols-2 gap-4">
-    <NuxtLink class="px-4 py-2 border !border-solid rounded-md hover:border-emerald-300" :to="prev ? prev._path : '/'">
-      <p class="text-gray-500 text-sm">{{ prev ? "上一章" : "📚" }}</p>
+    <NuxtLink 
+      class="px-4 py-2 border !border-solid dark:border-slate-600 rounded-md hover:border-emerald-300 dark:hover:border-emerald-600"
+      :to="prev ? prev._path : '/'">
+      <p class="text-gray-500 dark:text-gray-300 text-sm">{{ prev ? "上一章" : "📚" }}</p>
         {{ prev ? prev.title : '回到封面' }}
     </NuxtLink>
     <NuxtLink
-      class="px-8 py-2 border !border-solid rounded-md text-right hover:border-emerald-300"
+      class="px-8 py-2 border !border-solid dark:border-slate-600 rounded-md text-right hover:border-emerald-300 dark:hover:border-emerald-600"
       :to="next ? next._path : '/'"
     >
-      <p class="text-gray-500 text-sm">{{ next ? "下一章" : "📚" }}</p>
+      <p class="text-gray-500 dark:text-gray-300 text-sm">{{ next ? "下一章" : "📚" }}</p>
       {{ next ? next.title : '回到封面' }}
     </NuxtLink>
   </div>
