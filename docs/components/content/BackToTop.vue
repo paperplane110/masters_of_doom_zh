4abc45a624dd39aad8415486faadb410b059e29b
paperplane110 <1374736640@qq.com>
2024-07-12 21:03:38 +0800

    ch3,page51,p2

--- docs/components/content/BackToTop.vue
+++ docs/components/content/BackToTop.vue
@@ -2,7 +2,7 @@
     <div 
         v-if="isVisible"
         :class="[
-            'fixed top-[1rem] left-1/2 -translate-x-[50%]',
+            'fixed top-[1rem] left-1/2 -translate-x-[50%] z-10',
             'cursor-pointer flex items-center bg-gray-50 dark:bg-slate-900 px-4 py-2 border border-gray-400 dark:border-slate-600 rounded-lg shadow-md ',
             'hover:border-green-500 hover:text-green-500 dark:hover:border-green-500 dark:hover:text-green-500',
             'transition-all duration-300', 
@@ -39,5 +39,4 @@ onMounted(() => {
 onBeforeUnmount(() => {
     window.removeEventListener("scroll", handleScroll)
 })
-
 </script>
