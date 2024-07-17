2a789bceb199ff449d9f980192206deebc58d920
paperplane110 <1374736640@qq.com>
2024-07-17 12:57:32 +0800

    feat: add transition to backtotop

--- docs/components/content/BackToTop.vue
+++ docs/components/content/BackToTop.vue
@@ -1,18 +1,19 @@
 <template>
-    <div 
-        v-if="isVisible"
-        :class="[
-            'fixed top-[1rem] left-1/2 -translate-x-[50%] z-10',
-            'cursor-pointer flex items-center bg-gray-50 dark:bg-slate-900 px-4 py-2 border border-gray-400 dark:border-slate-600 rounded-lg shadow-md ',
-            'hover:border-green-500 hover:text-green-500 dark:hover:border-green-500 dark:hover:text-green-500',
-            'transition-all duration-300', 
-            { 'opacity-0': !isVisible }
-        ]"
-        @click="scrollToTop"
-    >
-        <Icon name="lets-icons:up" />
-        回到顶部
-    </div>
+    <Transition>
+        <div 
+            v-if="isVisible"
+            :class="[
+                'fixed top-[1rem] left-1/2 -translate-x-[50%] z-10',
+                'cursor-pointer flex items-center bg-gray-50 dark:bg-slate-900 px-4 py-2 border border-gray-400 dark:border-slate-600 rounded-lg shadow-md ',
+                'hover:border-green-500 hover:text-green-500 dark:hover:border-green-500 dark:hover:text-green-500',
+                'transition-all duration-300'
+            ]"
+            @click="scrollToTop"
+        >
+            <Icon name="lets-icons:up" />
+            回到顶部
+        </div>
+    </Transition>
 </template>
 
 <script setup>
@@ -40,3 +41,15 @@ onBeforeUnmount(() => {
     window.removeEventListener("scroll", handleScroll)
 })
 </script>
+
+<style>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.2s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
